2f186be79934f4d40ad2e68c8cf083fdb0888167
Sunil Shedge <51407211+surya88i@users.noreply.github.com>
2020-08-27 15:51:18 +0530

    Update conn.php

--- lib/conn.php
+++ lib/conn.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect('127.0.0.1','root','password','mydb');
+$con=mysqli_connect('localhost','root','password','mydb');
 if($con->connection_error){
 	echo "Connection failed:".$con->connection_error;
 }
